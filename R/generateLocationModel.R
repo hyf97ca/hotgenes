39d4c6c92458e43e41e3a19f62484ac225ee6f27
Yi Fei Huang <hyf97ca@gmail.com>
2019-10-04 02:18:27 -0400

    Add scaling to improve performance, add location model to improve usability

---
+++ R/generateLocationModel.R
@@ -0,0 +1,23 @@
+#' \code{generateLocationModels}
+#'
+#' @param startBase The starting base pair number
+#' @param endBase The ending base pair number
+#' @param scaling scaling factor in how many base pairs should be represented per index (ex. 1000) to reduce computational complexity when simulating physics/plotting;
+#' Should match value used in generateStrandModel
+#'
+#' @return A sequence containing the location model.
+#' @export
+#'
+#' @examples
+#'\dontrun
+#'{
+#'    generateLocationModel(1, 195471971)
+#'}
+#'
+
+
+generateLocationModel <- function(startBase, endBase, scaling=1000)
+{
+  return(t.(as.matrix(seq(from=startBase, to=endBase + scaling, by=scaling))))
+}
+
