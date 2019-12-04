c380a6ee1c98eb6e995cd43444b348d9e0886d19
Yi Fei Huang <hyf97ca@gmail.com>
2019-12-04 02:28:25 -0500

    edit shiny script to use faster function

--- inst/shiny-scripts/app.R
+++ inst/shiny-scripts/app.R
@@ -4,16 +4,24 @@ library(shiny)
 library(shinyWidgets)
 library(hotgenes)
 
+#DNA strand to be rendered start bp
+dnaStart <- 1
+#DNA strand to be rendered end bp
+dnaEnd <- 195471971
+#1 is most flexible, might speed up if use 1000; the scaling options must be divisible by this
+baseScale <- 1000
+
 ui <- fluidPage(
   sliderInput("range", "Start-End bases",
-              min = 1, max = 195471971,
-              value = c(1,195471971)),
+              min = dnaStart, max = dnaEnd,
+              value = c(dnaStart,dnaEnd)),
   sliderTextInput("scaling","Scaling",
-  choices=c(1, 100, 1000, 10000, 100000, 1000000, 1000000, 100000000),
+#  choices=c(1, 100, 1000, 10000, 100000, 1000000, 1000000, 100000000),
+  choices=c(1000, 10000, 100000, 1000000, 1000000, 100000000),
   selected=100000, grid = T),
   radioButtons("strand", "Strand (+/-)", c("+", "-")),
   numericInput("iterations", "Number of iterations to simulate", value=5),
-  numericInput("conductivity", "Heat 'conductivity'", min= 0, max = 1, value = 0.001),
+  numericInput("conductivity", "Heat 'conductivity'", min= 0, max = 1, value = 0.001, step = 0.001),
   plotOutput("heatedmap")
 )
 
@@ -21,18 +29,23 @@ server <- function(input, output) {
 
   sm <- reactive(
     {
-      hotgenes::generateStrandModel(startBase=input$range[1], endBase=input$range[2],
-                                    fc=musCh1fc, chr="chr1", strand=input$strand, scaling=input$scaling)
+      hotgenes::generateStrandModel(startBase=dnaStart, endBase=dnaEnd,
+                                    fc=musCh1fc, chr="chr1", strand=input$strand, scaling=baseScale)
     })
-  sm1 <- reactive(
+  smr <- reactive(
     {
-      hotgenes::simulateHeatSpread(sm(), input$conductivity, input$iterations)
+      hotgenes::rebuildStrandModel(strandModel=sm(), newStartBase=input$range[1], newEndBase=input$range[2], newScaling=input$scaling,
+                                   startBase=dnaStart, endBase=dnaEnd, scaling=baseScale)
+    })
+  smt <- reactive(
+    {
+      hotgenes::simulateHeatSpread(smr(), input$conductivity, input$iterations)
     })
   locations <- reactive({
      hotgenes::generateLocationModel(startBase=input$range[1], endBase=input$range[2], scaling=input$scaling)
   })
   output$heatedmap <- renderPlot({
-    hotgenes::plotHeatedMap(sm1(), locations())
+    hotgenes::plotHeatedMap(smt(), locations())
 
 
   })
